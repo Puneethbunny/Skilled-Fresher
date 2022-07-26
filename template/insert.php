44c4b5800b2590275920ebe1bc9f2ee960431617
Puneethbunny <107424399+Puneethbunny@users.noreply.github.com>
2022-07-26 22:32:40 +0530

    Update insert.php

--- template/insert.php
+++ template/insert.php
@@ -1,61 +1,26 @@
 <?php
-if (isset($_POST['submit'])) {
-    if (isset($_POST['firstname']) && isset($_POST['lastname']) &&
-        isset($_POST['corporatename']) && isset($_POST['email']) &&
-        isset($_POST['password'])) {
-        
-        $firstname = $_POST['firstname'];
-        $lastname = $_POST['lastname'];
-        $corporatename = $_POST['corporatename'];
-        $email = $_POST['email'];
-        $password = $_POST['password'];
+  $servername="63.250.60.129";
+  $username="vce1"; 
+  $password="!ntern\$hip2022"; 
+  $database="SF2";
 
-        $servername="63.250.60.129";
-        $username="vce1"; 
-  		$password="!ntern\$hip2022"; 
-  		$database="SF2";
+  $sfconn=new mysqli($servername, $username, $password, $database);
+  if($sfconn->connect_error){
+    die("connection failed: " . $sfconn->connect_error);
+  }
+  $firstname=$_POST['firstname'];
+  $lastname=$_POST['lastname'];
+  $corporatename=$_POST['corporatename'];
+  $email=$_POST['email'];
+  $password=$_POST['password'];
 
-  		$sfconn=new mysqli($servername, $username, $password, $database);
-  		if($sfconn->connect_error){
-    		die("connection failed: " . $sfconn->connect_error);
-        }
-        else {
-            $Select = "SELECT email FROM register WHERE email = ? LIMIT 1";
-            $Insert = "INSERT INTO register(firstname, lastname, corporatename, email, password) values(?, ?, ?, ?, ?)";
-
-            $stmt = $sfconn->prepare($Select);
-            $stmt->bind_param("s", $email);
-            $stmt->execute();
-            $stmt->bind_result($resultEmail);
-            $stmt->store_result();
-            $stmt->fetch();
-            $rnum = $stmt->num_rows;
-
-            if ($rnum == 0) {
-                $stmt->close();
-
-                $stmt = $sfconn->prepare($Insert);
-                $stmt->bind_param("ssssi",$firstname, $lastname, $corporatename, $email, $password);
-                if ($stmt->execute()) {
-                    echo "New record inserted sucessfully.";
-                }
-                else {
-                    echo $stmt->error;
-                }
-            }
-            else {
-                echo "Someone already registers using this email.";
-            }
-            $stmt->close();
-            $conn->close();
-        }
-    }
-    else {
-        echo "All field are required.";
-        die();
-    }
-}
-else {
-    echo "Submit button is not set";
-}
+  $sql = "INSERT INTO Corporates (firstname, lastname, corporatename, email, password) VALUES ('$firstname', '$lastname', '$corporatename', '$email', '$password')";
+  if (mysqli_query($sfconn, $sql)) {
+    header("Location: reg2.php?error=404 Not found");
+    exit();
+  }
+  else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($sfconn);
+  }
+  mysqli_close($sfconn);
 ?>
