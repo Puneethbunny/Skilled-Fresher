152c94e7e16a9185e5797e36e9589b267fd6d3f6
Puneethbunny <107424399+Puneethbunny@users.noreply.github.com>
2022-09-16 18:25:26 +0530

    Update indexx.php

--- template/indexx.php
+++ template/indexx.php
@@ -312,23 +312,12 @@ if(!$_SESSION['Email'])
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="index.html">
+            <a class="nav-link" href="candidates.php">
               <i class="icon-head menu-icon"></i>
               <span class="menu-title">Candidates</span>
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#tables" aria-expanded="false" aria-controls="tables">
-              <i class="icon-grid-2 menu-icon"></i>
-              <span class="menu-title">Tables</span>
-              <i class="menu-arrow"></i>
-            </a>
-            <div class="collapse" id="tables">
-              <ul class="nav flex-column sub-menu">
-                <li class="nav-item"> <a class="nav-link" href="pages/tables/basic-table.html">Basic table</a></li>
-              </ul>
-            </div>
-          </li>
+          
       </nav>
       <!-- partial -->
       <div class="main-panel">
